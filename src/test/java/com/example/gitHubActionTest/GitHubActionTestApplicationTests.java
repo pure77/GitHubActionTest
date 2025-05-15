fd8fb7b80b1cac3c72906b058a1829b201b4c725
pure77 <qkralsgus90@naver.com>
2025-05-15 18:52:48 +0900

    first commit

---
+++ src/test/java/com/example/gitHubActionTest/GitHubActionTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitHubActionTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubActionTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
